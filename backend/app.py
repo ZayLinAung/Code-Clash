8368759bf0c61230bf8e7fe04f8d1a5d2445785c
Jeffrey <jeffeyzhao2003@gmail.com>
2024-08-03 19:58:11 -0400

    change backend

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,39 @@
 from flask import Flask
-from flask_socketio import SocketIO
-from models import init_app
-from routes.room import blueprint as RoomRoute
-from routes.problem import blueprint as ProblemRoute
+from flask_socketio import SocketIO, emit
 
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = "the random string"
-app.config['MONGODB_URL'] = "mongodb://localhost:27017"
-
-init_app(app)
-app.register_blueprint(RoomRoute, url_prefix='/room')
-app.register_blueprint(ProblemRoute, url_prefix='/problem')
 socketio = SocketIO(cors_allowed_origins=["http://localhost:3000"])
-socketio.init_app(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def create_app():
+    app = Flask(__name__)
 
-@socketio.on('room:join')
-def join_room():
-    pass
+    app.config['SECRET_KEY'] = "the random string"
+    app.config['MONGODB_URL'] = "mongodb://localhost:27017"
 
-@socketio.on('room:start')
-def start_room():
-    pass
+    from models import init_app
+    from routes.room import blueprint as RoomRoute
+    from routes.problem import blueprint as ProblemRoute
+    #import events
 
+    init_app(app)
+    app.register_blueprint(RoomRoute, url_prefix='/room')
+    app.register_blueprint(ProblemRoute, url_prefix='/problem')
+    socketio.init_app(app)
+    return app
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: ' + msg)
+    emit('message', msg, broadcast=True)
+
+
+# @socketio.on('room:join')
+# def join_room():
+#     pass
+
+# @socketio.on('room:start')
+# def start_room():
+#     pass
+app = create_app()
 if __name__ == '__main__':
-    
-    socketio.run(app)
+    socketio.run(app, debug=True)
     #.run(debug=True)
