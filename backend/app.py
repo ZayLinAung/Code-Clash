3e8fcbb44319779f318a61d72e5c6e0b6ccb5656
Jeffrey <jeffeyzhao2003@gmail.com>
2024-08-03 19:39:53 -0400

    add sockets

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,33 @@
 from flask import Flask
+from flask_socketio import SocketIO
 from models import init_app
 from routes.room import blueprint as RoomRoute
 from routes.problem import blueprint as ProblemRoute
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "the random string"
 app.config['MONGODB_URL'] = "mongodb://localhost:27017"
 
 init_app(app)
-
 app.register_blueprint(RoomRoute, url_prefix='/room')
 app.register_blueprint(ProblemRoute, url_prefix='/problem')
+socketio = SocketIO(cors_allowed_origins=["http://localhost:3000"])
+socketio.init_app(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+@socketio.on('room:join')
+def join_room():
+    pass
+
+@socketio.on('room:start')
+def start_room():
+    pass
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    socketio.run(app)
+    #.run(debug=True)
